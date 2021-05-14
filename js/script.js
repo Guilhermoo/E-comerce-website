6ef55dbc15c7084b7ed6e8a5e3742835040bd2aa
Guilherme Rubio <SchellingWelling@gmail.com>
2021-05-14 15:59:14 -0300

    header pronto!

--- js/script.js
+++ js/script.js
@@ -4,10 +4,19 @@ const navClose = document.querySelector('.close');
 const navBar = document.querySelector('.nav')
 const navLeft = menu.getBoundingClientRect().left;
 
+/* Abrindo o menu */
 navOpen.addEventListener('click', ()=>{
-    if (navLeft < 0){
-        menu.classList.add('show');
-        document.body.classList.add('show');
-        navBar.classList.add('show');
-    }
+    console.log('aberto')
+    menu.classList.add('show');
+    document.body.classList.add('show');
+    navBar.classList.add('show');
+})
+
+/* fechando o menu */
+
+navClose.addEventListener('click', ()=>{
+    console.log('fechado');
+    menu.classList.remove('show');
+    document.body.classList.remove('show');
+    navBar.classList.remove('show')
 })
